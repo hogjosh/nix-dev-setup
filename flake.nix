2456080004250fa14979cd48b8a1ed032159aa1b
Josh Hogan <josh.hogan@me.com>
2026-05-17 12:08:49 -0400

    Add NUR input for community packages

--- flake.nix
+++ flake.nix
@@ -7,15 +7,20 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    { nixpkgs, home-manager, nur, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         config.allowUnfree = true;
         inherit system;
+        overlays = [ nur.overlays.default ];
       };
     in
     {
